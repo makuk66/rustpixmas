5fbe0c295352ba60716638ae59f0e99495a98a0d
Martijn Koster <mak-github@greenhills.co.uk>
2018-12-02 22:57:26 +0000

    replace repeated ranges with single Vec

--- src/main.rs
+++ src/main.rs
@@ -10,30 +10,27 @@ use rand::Rng;
 use rppal::gpio::{Gpio, Level, Mode};
 use rppal::system::DeviceInfo;
 
-const LED_START: u8 = 2;
-const LED_END: u8 = 27;
-
-fn switch_all_leds(gpio: &mut Gpio, level: Level) {
+fn switch_all_leds(gpio: &mut Gpio, leds: & Vec<u8>, level: Level) {
     println!("switching the lights to {}", level);
-    for gpio_led_number in LED_START..LED_END {
-        gpio.write(gpio_led_number, level);
+    for gpio_led_number in leds {
+        gpio.write(*gpio_led_number, level);
     }
 }
 
-fn configure_leds(gpio: &mut Gpio) {
+fn configure_leds(gpio: &mut Gpio, leds: & Vec<u8>) {
     println!("configuring gpio");
-    for gpio_led_number in LED_START..LED_END {
-        gpio.set_mode(gpio_led_number, Mode::Output);
+    for gpio_led_number in leds {
+        gpio.set_mode(*gpio_led_number, Mode::Output);
     }
 }
 
-fn random_leds(rng: &mut StdRng, gpio: &mut Gpio) {
-    for gpio_led_number in LED_START..LED_END {
+fn random_leds(rng: &mut StdRng, gpio: &mut Gpio, leds: & Vec<u8>) {
+    for gpio_led_number in leds {
         let value = match rng.gen_ratio(18, 20) {
             true => Level::High,
             false => Level::Low,
         };
-        gpio.write(gpio_led_number, value);
+        gpio.write(*gpio_led_number, value);
     }
 }
 
@@ -51,13 +48,15 @@ fn main() {
 
     let mut gpio = Gpio::new().unwrap();
 
-    configure_leds(&mut gpio);
+    let leds: Vec<u8> = (2..27).collect();
 
-    switch_all_leds(&mut gpio, Level::Low);
+    configure_leds(&mut gpio, &leds);
+
+    switch_all_leds(&mut gpio, &leds, Level::Low);
 
     thread::sleep(Duration::from_secs(1));
 
-    switch_all_leds(&mut gpio, Level::High);
+    switch_all_leds(&mut gpio, &leds, Level::High);
 
     thread::sleep(Duration::from_secs(2));
 
@@ -65,7 +64,7 @@ fn main() {
     let mut rng: StdRng = SeedableRng::from_seed(*b"jingle_bells_jingle_all_the_way!");
 
     loop {
-        random_leds(&mut rng, &mut gpio);
+        random_leds(&mut rng, &mut gpio, &leds);
         thread::sleep(Duration::from_millis(200));
     }
 }
