b6d19e97639ffbf4f44569f049a95a6d55c8af45
Martijn Koster <mak-github@greenhills.co.uk>
2018-12-06 22:56:39 +0000

    switch to Rust 2018
    I rebuilt the cross compiler with: docker build --build-arg PI_TOOLS_GIT_REF=master --build-arg RUST_VERSION=stable --tag rust:2018 .

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 docker run \
     --volume "$PWD:/home/cross/project" \
     --volume "$HOME/.cargo/registry>:/home/cross/.cargo/registry" \
-    ragnaroek/rust-raspberry:1.30.0 \
+    rust:2018 \
     build
 
 scp ./target/arm-unknown-linux-gnueabihf/debug/rustpixmas pi@${PI_HOST:-pi}:
